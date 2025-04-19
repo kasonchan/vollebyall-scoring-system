24b0b4c626fbf45ac9ae2323636817d82bda018c
kasonchan <kasonl.chan@gmail.com>
2025-04-19 01:16:30 +0100

    Fix typo

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: 'vollebyall-scoring-system',
   plugins: [react()],
   resolve: {
     alias: {
